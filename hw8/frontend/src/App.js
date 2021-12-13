47c291ffb9db3ad29f8aa2cfaa09fb03c7ff1ff6
Edward Ong <edward@MSI.localdomain>
2021-12-13 16:23:13 +0800

    update hw8

--- hw8/frontend/src/App.js
+++ hw8/frontend/src/App.js
@@ -1,13 +1,22 @@
 import './App.css'
 import { Button, Input, Tag, message } from 'antd'
-import useChat from './useChat'
-import { useRef, useState, useEffect } from 'react'
+import {useState, useEffect, useRef} from 'react';
+import useChat from './useChat';
+import { UserOutlined } from "@ant-design/icons";
+
+const LOCALSTORAGE_KEY = "save-me";
 
 function App() {
-  const { status, messages, sendMessage, clearMessage } = useChat()
-  const [username, setUsername] = useState('')
-  const [body, setBody] = useState('')
-  const bodyRef = useRef(null)
+  const savedMe = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  const { status, messages, sendMessage, clearMessage } = useChat();
+  const [username, setUsername] = useState('');
+  const [body, setBody] = useState(''); 
+  const [signedIn, setSignedIn] = useState(false);
+  const [me, setMe] = useState(savedMe || "");
+
+  const bodyRef = useRef(null);
+
   const displayStatus = (payload) => {
     if (payload.msg) {
       const { type, msg } = payload
@@ -17,65 +26,92 @@ function App() {
         case 'success':
           message.success(content)
           break
-        case "error":
+        case 'error':
         default:
-          message.success(content)
+          message.error(content)
           break
   }}}
 
-  useEffect(()=>{
-    displayStatus(status)},[status])
+  useEffect( () => {
+    displayStatus(status);
+  }, [status])
 
-  return (
-    <div className="App">
+  useEffect(() => {
+    if (signedIn) {
+      localStorage.setItem(LOCALSTORAGE_KEY, me);
+    }
+  }, [signedIn, me]);
+
+
+  if (signedIn) {
+    return (
+      <div className="App">
+        <div className="App-title">
+          <h1>{me}'s Chat Room</h1>
+          <Button type="primary" danger onClick={clearMessage} >
+            Clear
+          </Button>
+        </div>
+
+        <div className="App-messages">
+          {messages.length === 0 ? (
+            <p style={{ color: '#ccc' }}> No messages... </p>
+          ) : (
+            messages.map(({ name, body }, i) => (
+              <p className="App-message" key={i}>
+                <Tag color="blue">{name}</Tag> {body}
+              </p>
+            ))
+          )}
+        </div>
+
+        <Input.Search
+          ref={bodyRef}
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          enterButton="Send"
+          placeholder="Type a message here..."
+          onSearch={(msg) => {
+            if (!msg || !username) {
+              displayStatus({
+                type: 'error',
+                msg: 'Please enter a username and a message body.'
+              })
+            }    
+            sendMessage({ name: username, body: msg })
+            setBody('')
+          }}
+        ></Input.Search>
+      </div>
+    )
+  } 
+  else return (
+    <>
       <div className="App-title">
-        <h1>Simple Chat</h1>
-        <Button type="primary" danger onClick = {clearMessage}>
-          Clear
-        </Button>
-      </div>
-      <div className="App-messages">
-        {messages.length === 0 ? ( 
-        <p style={{ color: '#ccc' }}>
-          No messages...
-        </p>):(
-          messages.map(({ name, body }, i) => (
-            <p className = "App-messages" key = {i}>
-              <Tag color = "blue">{name}</Tag> {body}
-            </p>
-          ))
-        )}
-      </div>
-      <Input
-        placeholder="Username"
-        value = {username}
-        onChange = {(e) => setUsername(e.target.value)}
-        style={{ marginBottom: 10 }}
-        onKeyDown = {(e) => {
-          if(e.key === 'Enter') bodyRef.current.focus() 
-        }}>
-        </Input>
+        <h1>My Chat Room</h1>
+      </div>  
       <Input.Search
-        value = {body}
-        ref = {bodyRef}
-        onChange = {(e) => setBody(e.target.value)}
-        enterButton="Send"
-        placeholder="Type a message here..."
-        onSearch = {(msg) => {
-          if(!msg||!username)
-          {
-            displayStatus({
-              type: 'error',
-              msg: 'Please enter a username and a message body.'
-            })
-            return;
+        classname="Input-username"
+        prefix={<UserOutlined />}
+        value={me} enterButton="Sign In"
+        onChange={(e) => setMe(e.target.value)}
+        placeholder="Enter your name"
+        size="large" style={{ width: 300, margin: 50 }}
+        onSearch={ (name) => {
+          if (!name) {
+              displayStatus({
+                  type: "error",
+                  msg: "Missing user name",
+              });
           }
-          sendMessage({ name:username, body:msg })
-          setBody('')
-        }}
-      ></Input.Search>
-    </div>
+          else {
+            setSignedIn(true);
+            setUsername(me)
+          }
+        } }
+      />
+    </>
   )
 }
 
-export default App
+export default App;
